592ced56263482858ef974c3f52d0a1c7f723575
its1801f2219 <66263644+pumpinsatyr916@users.noreply.github.com>
2022-11-29 15:37:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+#practice commands for linux shell
+
+#shell is a program that can be used to run comamands
+#usually called system commands 
+
+#~/cli-01$ is called a prompt
+
+# ~ represents the home directory
+# / is a separator for directories and or files 
+# cil-01 is another directory 
+
+# is is for listing files and directories in the current directory
+
+#ls-l is for long list formant 
+# the first letter indicated if it is a directory(d) or a file(-)
+
+#ls -al is for long format and showing hidden files 
+
+# hidden files, have names that start with a dot (.)
+
+#cd is for changing directorires (notice that the prompt also relfects the change)
+
+
+# to go up one directory:
+# cd ..
+
+#shells usually have autocomplete using the TAB key 
+
+#pwd (presend working directoiory) and shows the full path to the directory you are currenty in 
+
+#cat command (cat stnds for catalog)
+#to show the contents of a text file (files that has characters that are printiable on the screen)
